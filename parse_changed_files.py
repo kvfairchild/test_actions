152b14b65c764dc15fb1c34719f760058414b8d0
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-11-01 03:22:34 -0400

    automerge

--- parse_changed_files.py
+++ parse_changed_files.py
@@ -1,18 +1,18 @@
-import logging
 import sys
 
 PLUGIN_DIRS = ['benchmarks', 'data', 'models', 'metrics']
 
 changed_files = sys.argv[1]
+changed_files_list = changed_files.split()
+
 plugin_files_changed = []
 non_plugin_files_changed = []
-for f in changed_files:
+
+for f in changed_files_list:
 	if not any(plugin_dir in f for plugin_dir in PLUGIN_DIRS):
-    	print(f)
-    	non_plugin_files_changed.append(f)
-    else:
-    	print(f"plugin file {f} changed")
-    	plugin_files_changed.append(f)
+		non_plugin_files_changed.append(f)
+	else:
+		plugin_files_changed.append(f)
 
 if len(non_plugin_files_changed) > 0:
 	print("stop")
